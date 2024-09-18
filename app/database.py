dd58cca6ccdf112ff3241dd2f9e49ee971fdb9ed
EduardaAgnesDeveloper <142688464+EduardaAgnesDeveloper@users.noreply.github.com>
2024-09-17 21:58:07 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import create_engine, Session, SQLModel
 
-DATABASE_URL = "sqlite:///./test.db"  # Alterar para a URL do banco de dados
+DATABASE_URL = "sqlite:///test.db"  # Alterar para a URL do banco de dados
 
 engine = create_engine(DATABASE_URL, echo=True)
 
